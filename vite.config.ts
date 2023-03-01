68dae0b1d655f5b3233876f9ad3f401326cebac0
Brian Petersen <skiwalk3r@gmail.com>
2023-03-01 16:32:49 -0700

    Corrects site path for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Needed for github pages deploy
+  base: '/brotips/',
   plugins: [react()],
 })
